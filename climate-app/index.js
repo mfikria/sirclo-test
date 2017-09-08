2bc97dbd8583c5171a106d159a74979b9ae7fb47
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-09-08 14:03:44 +0700

    Fecth the data

--- climate-app/index.js
+++ climate-app/index.js
@@ -1,4 +1,9 @@
 function fetchData() {
 	var cityId = document.forms["climate-form"]["city-list"].value;
-	alert(cityId);
+
+	fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q=' + cityId + '&mode=json&units=metric&cnt=5&APPID=481e3bc28e5264e5607c2b65b449bfc1', {method: 'GET'}).then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		console.dir(data);
+	});
 }
